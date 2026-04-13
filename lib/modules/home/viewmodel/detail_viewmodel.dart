dae85e939727ef2174385cde93bbf23917556e48
Taha Shehzad <tahashehzad063@gmail.com>
2026-04-13 16:31:04 +0500

    refactor: Improve code formatting and structure in DetailView and DetailViewModel

--- lib/modules/home/viewmodel/detail_viewmodel.dart
+++ lib/modules/home/viewmodel/detail_viewmodel.dart
@@ -1,13 +1,10 @@
 import 'package:get/get.dart';
 
 class DetailViewModel extends GetxController {
-  // Reactive quantity counter
   RxInt quantity = 2.obs;
 
-  // Reactive selected size index (0=10", 1=14", 2=16")
   RxInt selectedSizeIndex = 1.obs;
 
-  // Reactive favorite toggle
   RxBool isFavorite = false.obs;
 
   void incrementQuantity() {
